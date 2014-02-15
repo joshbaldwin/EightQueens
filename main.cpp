5fe043bc7164501225b63aeb406e254364cd26f8
joshbaldwin <josh.baldwin.42@gmail.com>
2014-02-14 22:20:03 -0500

    initial program works.  now for touch ups

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include <string>
 #include <stdlib.h>
 #include <cmath>
+#include <ctime>
 using namespace std;
 
 // Define maximum population size.
-const int maxPopulation = 1;
+const int maxPopulation = 20;
 
 // Define maximum generation size.
-const int maxGeneration = 15;
+const int maxGeneration = 20;
 
 // A chess board is 8x8.
 const int boardSize = 8;
@@ -23,11 +24,19 @@ class Organism {
 		void fitnessTest();
 		void mutate();		
 		string intToString(int i);
+		Organism* operator=(Organism* src);
 };
 
 Organism::Organism() {
 	state = generateState();
 	fitnessNumber = 0;
+}
+
+Organism* Organism::operator=(Organism* src) {
+	Organism* desc = new Organism();
+	desc->fitnessNumber = src->fitnessNumber;
+	desc->state = src->state;
+	return desc;
 }
 
 string Organism::intToString(int i) {
@@ -73,7 +82,7 @@ void Organism::fitnessTest() {
 	for(int i=0; i<8; i++) {
 		diagonalIndex = 0;
 		currentQueenState = state.substr(i,1);
-		cout << "i=" << i << " state=" << state << " current=" << currentQueenState << endl;
+		//cout << "i=" << i << " state=" << state << " current=" << currentQueenState << endl;
 		for(index=i+1; index<8; index++) {
 			diagonalIndex = index - i;
 			nextQueenState = state.substr(index, 1);
@@ -103,9 +112,18 @@ void Organism::fitnessTest() {
 
 void Organism::mutate() {
 	// Mutate one random gene in the organism (one random queen in the state string).
-	string genePosition = intToString( rand() % 8 );
+	int genePosition = rand() % 8;
+	string newGene = intToString( rand() % 8 + 1);
 	
+	if(genePosition == 0) {
+		state = newGene + state.substr(genePosition + 1);
+	} else if (genePosition == 7 ) {
+		state = state.substr(0, genePosition) + newGene;
+	} else {
+		state = state.substr(0, genePosition) + newGene + state.substr(genePosition + 1);
+	}
 }
+
 
 class Population {
 	public:
@@ -115,6 +133,10 @@ class Population {
 
 		Population(int maxPop, int maxGen);
 		~Population();
+		void evolve();
+		void printPop();
+	private:
+		void sort();
 };
 
 Population::Population(int maxPop, int maxGen) {
@@ -132,9 +154,92 @@ Population::~Population() {
 	delete organisms;
 }
 
+void Population::sort() {
+	bool flag = true;
+	Organism* tempOrganism;
+	int j;
+	while(flag) {
+		flag = false;  // Assume no swap happened.
+		for(j=0; j<maxPopulation-1; j++) {
+			if(organisms[j]->fitnessNumber < organisms[j+1]->fitnessNumber ) {
+				tempOrganism = organisms[j]; // the swap element.
+				organisms[j] = organisms[j+1];
+				organisms[j+1] = tempOrganism;
+				flag = true; // a swap occurred, so we continue.
+			}
+		}
+	}
+}
+
+void Population::printPop() {
+	for(int i=0; i<maxPopulation; i++) {
+		cout << "state=" << organisms[i]->state << " fitnessNumber=" 
+			<< organisms[i]->fitnessNumber << endl;
+	}
+}
+
+void Population::evolve() {
+	// Select the top half.
+	int selectionNumber = ceil(maxPopulation / 2.0 );
+
+	// These will store the front and back half of the genome.
+	string* frontGenes = new string[selectionNumber];
+	string* backGenes = new string[selectionNumber]; 
+
+	int front, back;
+
+	for(int i=0; i<maxGeneration; i++) {
+		// Filter the best organisms to the top of the array.
+		sort();
+
+		// Here we split each genome in half to have the front and back genes.
+		for(int j=0; j<selectionNumber; j++) {
+			frontGenes[j] = (organisms[j]->state).substr(0,4);
+			backGenes[j] = (organisms[j]->state).substr(4,8);
+		}
+
+		// Mate by randomly swapping around the front and back genes.
+		for(int j=0; j<maxPopulation; j++) {
+			front = rand() % selectionNumber;
+			back = rand() % selectionNumber;
+			while(front == back) {
+				back = rand() % selectionNumber;
+			}
+			organisms[j]->state = frontGenes[front] + backGenes[back];
+		}
+
+		// Mutate.
+		for(int j=0; j<maxPopulation; j++) {
+			organisms[j]->mutate();
+		}
+
+
+		for(int j=0; j<maxPopulation; j++) {
+			organisms[j]->fitnessTest();
+			if(organisms[j]->fitnessNumber == 28 ) {
+				// We have a winner.  Kick out of the main loop.
+				// Assumes maxGeneration < 1000000;
+				cout << "*****" << organisms[j]->state << "*******" << endl;
+				i = j = 1000000;
+				break;
+			}
+		}
+	}
+
+	delete[] frontGenes;
+	delete[] backGenes;
+}
+
 int main(int argc, char* argv[]) {
+	// Ensure our rand() is a bit more random than normal.
+	srand( time(0));
 
 	Population *population = new Population( maxPopulation, maxGeneration );
-	
+	population->printPop();
+	cout << endl;
+	//population->printPop();
+	population->evolve();
+population->printPop();
+
 	return 0;
 }
